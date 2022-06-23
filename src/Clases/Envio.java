f3b2725b1218284596d342673986ee4e6d92b22d
LucianoIsgro <88854992+LucianoIsgro@users.noreply.github.com>
2022-06-23 01:00:11 -0300

    Update Envio.java

--- src/Clases/Envio.java
+++ src/Clases/Envio.java
@@ -1,7 +1,7 @@
 package Clases;
 
 public class Envio {
-	 public static String codigoEnvio = "10A34JF12S";
+	 public static String codigoEnvio = "10A34JF12";
 	 public static String[] observaciones = {"Observacion1", "a", "b"};
 	 public static String estado = "Pendiente";
 	 public static double precio = 800.17;
